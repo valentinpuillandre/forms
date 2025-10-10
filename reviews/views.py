4f8a4ee6b9f377687bcadfe51e699154e44c9c5b
Valentin <valentinpuillandrepro@gmail.com>
2025-10-10 19:22:15 +0200

    form validation

--- reviews/views.py
+++ reviews/views.py
@@ -9,7 +9,8 @@ def review(request):
         if form.is_valid():
             print(form.cleaned_data)
             return HttpResponseRedirect("/thank-you")
-    form = ReviewForm()
+    else:
+        form = ReviewForm()
     return render(request, "reviews/review.html", { "form": form})
 
 def thank_you(request):
