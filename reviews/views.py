95018e4831a762ec977a0d44a4bfd4dc1d7ac6f2
Valentin <valentinpuillandrepro@gmail.com>
2025-10-13 10:27:31 +0200

    Using ModelForm

--- reviews/views.py
+++ reviews/views.py
@@ -8,12 +8,7 @@ def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating']
-            )
-            review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm()
