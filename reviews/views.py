b1a8fba4f32c6498446fe8ffb420932588d84817
Valentin <valentinpuillandrepro@gmail.com>
2025-10-13 16:32:33 +0200

    Using TemplateView

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,11 @@
+from typing import Any
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import ReviewForm
 from django.views import View
+from django.views.generic.base import TemplateView
+
+from .models import Review
+from .forms import ReviewForm
 
 class ReviewView(View):
     def get(self, request):
@@ -22,7 +26,29 @@ class ReviewView(View):
             "form": form
         })
 
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
 
-class ThankYouView(View):
-    def get(self, request):
-        return render(request, "reviews/thank_you.html")
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+class ReviewListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews
+        return context
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        selected_review = Review.objects.get(pk=review_id)
+        context["review"] = selected_review
+        return context
