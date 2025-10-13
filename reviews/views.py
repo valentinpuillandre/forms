23d5ed9fc0aafc4a45c5480dc9bfd3fb1682ab9d
Valentin <valentinpuillandrepro@gmail.com>
2025-10-13 09:57:30 +0200

    add database

--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
+from .models import Review
 
 
 def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            review = Review(
+                user_name=form.cleaned_data['user_name'],
+                review_text=form.cleaned_data['review_text'],
+                rating=form.cleaned_data['rating']
+            )
+            review.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm()
