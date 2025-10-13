7a57307827faa18de1eb054eadd96d21fb58c68d
Valentin <valentinpuillandrepro@gmail.com>
2025-10-13 16:40:59 +0200

    Using ListView

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .models import Review
 from .forms import ReviewForm
@@ -34,14 +36,15 @@ class ThankYouView(TemplateView):
         context["message"] = "This works!"
         return context
 
-class ReviewListView(TemplateView):
+class ReviewListView(ListView):
     template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    # def get_queryset(self) -> QuerySet[Any]:
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=3)
+    #     return data
 
 class SingleReviewView(TemplateView):
     template_name = "reviews/single_review.html"
