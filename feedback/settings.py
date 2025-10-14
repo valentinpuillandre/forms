2c39115149d97feeb96b9d7874a6a62d5451e697
Valentin <valentinpuillandrepro@gmail.com>
2025-10-14 10:55:40 +0200

    save file to db

--- feedback/settings.py
+++ feedback/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+MEDIA_ROOT = BASE_DIR / "uploads"
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,26 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
+from .forms import ProfileForm
+from .models import UserProfile
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        pass
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "profiles/create_profile.html", {
+                "form": submitted_form
+            })
